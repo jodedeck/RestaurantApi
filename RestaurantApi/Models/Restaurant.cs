51bf427a51f9849c2bace5b4cf73fb32df36b1b8
Jonathan de Decker <jodedeck542@gmail.com>
2019-02-18 12:02:41 +0100

    test

--- RestaurantApi/Models/Restaurant.cs
+++ RestaurantApi/Models/Restaurant.cs
@@ -11,6 +11,6 @@ namespace RestaurantApi.Models
         public string Name { get; set; }
         public string Address { get; set; }
         public string Description { get; set; }
-
+        public int PhoneNumber { get; set; }
     }
 }
